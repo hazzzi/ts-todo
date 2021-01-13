7f52ecbeccfb2eadc57be72e3a528ddb16994f28
hazzzi <hanee_l@naver.com>
2021-01-13 20:15:19 +0900

    todo 추가 기능

--- src/controllers/todos/index.ts
+++ src/controllers/todos/index.ts
@@ -10,3 +10,24 @@ const getTodos = async (request: Request, response: Response): Promise<void> => 
         throw error
     }
 }
+
+const addTodo = async (request: Request, response: Response): Promise<void> => {
+    try {
+        const body = request.body as Pick<ITodo, "name" | "description" | "status">
+
+        const todo: ITodo = new Todo({
+            name: body.name,
+            description: body.description,
+            status: body.status,
+        })
+
+        const newTodo: ITodo = await todo.save()
+        const allTodos: ITodo[] = await Todo.find()
+
+        response
+            .status(201)
+            .json({ message: "Todo added", todo: newTodo, todos: allTodos })
+    } catch (error) {
+        throw error
+    }
+}
