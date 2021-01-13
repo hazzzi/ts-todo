e35c20875c47de2c56526be2057638a552e61bed
hazzzi <hanee_l@naver.com>
2021-01-13 20:54:53 +0900

    app 수정

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
-import express, { Request, Response, NextFunction } from "express";
-import router from "./router/router";
+import express, { Express } from "express";
+import mongoose from "mongoose"
+import cors from "cors"
+import todoRoutes from "./router/router"
 
-const app = express();
+const app: Express = express();
+const PORT: string | number = process.env.PORT || 4000
 
-app.get('/', (request: Request, response: Response) => {
-    response.send('hello');
-})
+app.use(cors())
+app.use(todoRoutes)
 
-app.use(express.static('public'))
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.4q1gs.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.set("useFindAndModify", false)
 
-app.listen(3000, () => {
-    console.log('start server');
-})
+mongoose
+    .connect(uri, options)
+    .then(() => {
+        app.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`))
+    })
+    .catch(error => {
+        throw error
+    })
